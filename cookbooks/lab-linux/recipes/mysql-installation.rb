be404a3df4ae5f755222f9e85f65180614ee2a57
Ghanghoriya <jyoti.ghanghoriya@sap.com>
2020-09-18 15:17:44 +0530

    m1

--- cookbooks/lab-linux/recipes/mysql-installation.rb
+++ cookbooks/lab-linux/recipes/mysql-installation.rb
@@ -8,9 +8,17 @@ execute "apt-get-update" do
     command "apt-get update"
 end
 
-packages = ["mysql-server" , "mysql-client" , "libmysql-java"]
+packages = ["mysql-server" , "mysql-client"]
 packages.each do |i|
     package i do
         action :install
     end
+end
+
+apt_update 'update' do
+    action :update
+end
+
+package "libmysql-java" do 
+    action :install 
 end
