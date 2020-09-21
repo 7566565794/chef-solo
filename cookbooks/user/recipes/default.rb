ba4b38cb2b9fc2f6ed34691681ae79c7e4497c41
Ghanghoriya <jyoti.ghanghoriya@sap.com>
2020-09-21 08:52:23 +0530

    first

---
+++ cookbooks/user/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook:: user
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+user "jyoti" do
+    comment "this is a demo user created for checking databags"
+    uid 2000 
+    home '/home/jyoti'
+    shell '/bin/bash'
+    password abc@123
+end
