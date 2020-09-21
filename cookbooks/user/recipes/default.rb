75f51d31e73c4667c4ae33dd202f5d9324ab8857
Ghanghoriya <jyoti.ghanghoriya@sap.com>
2020-09-21 15:35:11 +0530

    m1

--- cookbooks/user/recipes/default.rb
+++ cookbooks/user/recipes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
 jyoti_password=data_bag_item('user_password','jyoti')
+puts(jyoti_password)
 user "jyoti" do
     comment "this is a demo user created for checking databags"
     uid 2000 
